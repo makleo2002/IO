8c4f798ba908a6a65907b60e7c877f40d99361be
makleo2002 <75272208+makleo2002@users.noreply.github.com>
2022-05-15 13:34:42 +0300

    Add files via upload

---
+++ IO LR2/IO LR2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define INF INT_MAX
+
+#define V 6   //количество вершин в графе
+
+int G[V][V] = {    //матрица смежности
+  {0,7,8,0,0,0},
+  {7,0,11,2,0,0},
+  {8,11,0,6,9,0},
+  {0,2,6,0,11,9},
+  {0,0,9,11,0,10},
+  {0,0,0,9,10,0}
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int min_edge = 0;           // количество ребер в мин.остовном дереве
+
+
+    bool selected[V+1];  //массив для отслеживания выбранной вершины
+
+   
+    memset(selected, false, sizeof(selected)); // изначально выбранное значение false
+
+ // количество ребер в минимальном остовном дереве будет
+ // всегда (V-1), где V - количество вершин в графе
+ // выбераем 1-ю вершину и делаем ее истинной
+    selected[0] = true;
+
+    int x;            //  номер строки
+    int y;            //  номер колонки
+    int tree_weight=0; //суммарный вес остова
+
+    cout << "Ребро" << " : " << "Вес";
+    cout << endl;
+    while (min_edge < V - 1) {
+
+        int min = INF;
+        x = 0;
+        y = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (selected[i]) {
+                for (int j = 0; j < V; j++) {
+                    if (!selected[j] && G[i][j]) { // не выбранное и есть ребро
+                        if (min > G[i][j]) {
+                            min = G[i][j];
+                            x = i;
+                            y = j;
+                        }
+
+                    }
+                }
+            }
+        }
+        tree_weight += G[x][y];
+        cout << x+1 << " - " << y+1 << " :  " << G[x][y];
+        cout << endl;
+        selected[y] = true;
+        min_edge++;
+    }
+    cout << "Вес минимального остова: "<<tree_weight;
+    return 0;
+}
---
+++ IO LR3/IO LR3.cpp
@@ -0,0 +1,90 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define n 6
+
+int G[6][6]{
+{0, 1, 1, 0, 0, 0},
+{1, 0, 1, 1, 0, 0},
+{1, 1, 0, 1, 1, 0},
+{0, 1, 1, 0, 1, 1},
+{0, 0, 1, 1, 0, 1},
+{0, 0, 0, 1, 1, 0}
+};
+
+void Lee(int S, int F) {
+    S--;
+    F--;
+    int length, wawe[n];
+    vector<int> NewFront, OldFront;
+    for (int i = 0; i < n; i++)
+        wawe[i] = -1;
+
+ 
+    
+    wawe[S] = 0;
+    cout << "Wawe: " << endl;
+    for (int k = 0; k < n; k++) {
+        cout << wawe[k] << " ";
+    }
+    cout << endl;
+    OldFront.push_back(S);
+    cout << "NewFront: {} " << endl;
+    cout << "OldFront: " <<S+1<< endl; 
+    length = 0;
+    cout << "length " << length<<endl;
+    while (true) {
+        for (int i = 0; i < OldFront.size(); i++) {
+            for (int j = 0; j < n; j++) {
+                if (G[OldFront[i]][j] == 1) {
+                    if (wawe[j] == -1) {
+                        wawe[j]++;
+                        
+                    }
+
+                    NewFront.push_back(j);
+                }
+            }
+        }
+        cout << "Wawe: " << endl;
+        for (int k = 0;k<n; k++) {
+            cout <<wawe[k]<<" ";
+        }
+        cout << endl;
+        cout << "NewFront: " << endl;
+        for (int k = 0; k < NewFront.size(); k++) {
+           cout << NewFront[k]+1 << " ";
+        }
+        cout << endl;
+        cout << "OldFront: " << endl;
+        for (int k = 0; k < OldFront.size(); k++) {
+            cout << OldFront[k]+1 << " ";
+        }
+        cout << endl;
+        cout << "length " << length+1;
+        cout << endl;
+        if (NewFront.empty()) {
+            cout << "Нет пути";
+            break;
+        }
+        for (int m = 0; m < NewFront.size(); m++) {
+            if (NewFront[m] == F) {
+                cout << "Путь найден" << endl;
+                cout << "Длина пути " << length + 1;
+                return;
+            }
+        }
+        OldFront = NewFront;
+        NewFront = {};
+        length++;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Lee(1, 6);
+}
+
